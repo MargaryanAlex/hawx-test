6cfb47dc322cde5f68baa06c9b0ad3c1c00d09f6
Alex Margaryan <margryanax@gmail.com>
2023-01-19 19:11:05 +0400

    fix

--- src/components/header/components/desktop-header/index.tsx
+++ src/components/header/components/desktop-header/index.tsx
@@ -22,7 +22,8 @@ const DesktopHeader = ({
         setOpacity(1)
         setLeft(element.offsetLeft + (element.offsetWidth / 2))
     }
-    const checkRoute = () => {
+    useEffect(() => {
+
         let index = navList.findIndex(item => location.pathname === item.path)
 
         if (index >= 0 && setPosition) {
@@ -42,10 +43,28 @@ const DesktopHeader = ({
             setTimeout(() => setPosition(elementsRef?.current[subIndex as number].current as HTMLElement), 0)
 
         }
-    }
-    useEffect(() => {
-        checkRoute()
-        window.addEventListener("resize", checkRoute)
+
+        window.addEventListener("resize", () => {
+            let index = navList.findIndex(item => location.pathname === item.path)
+
+            if (index >= 0 && setPosition) {
+                setTimeout(() => setPosition(elementsRef?.current[index].current as HTMLElement), 0)
+            }
+            let subIndex: number | undefined = undefined
+            navList.map((item, index) => {
+                if (item.subList) {
+                    item.subList.map(route => {
+                        if (route.path === location.pathname) {
+                            subIndex = index
+                        }
+                    })
+                }
+            })
+            if (subIndex && subIndex >= 0 && setPosition) {
+                setTimeout(() => setPosition(elementsRef?.current[subIndex as number].current as HTMLElement), 0)
+
+            }
+        })
     }, [])
 
 
