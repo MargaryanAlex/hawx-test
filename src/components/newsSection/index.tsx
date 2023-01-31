768eb02304d3532e4aeffed7ddb589b3e8e5d17b
Alex Margaryan <margryanax@gmail.com>
2023-01-31 10:29:06 +0400

    connected back-test

--- src/components/newsSection/index.tsx
+++ src/components/newsSection/index.tsx
@@ -88,7 +88,7 @@ const NewsSection = () => {
 
 
     useEffect(() => {
-        axios.get("http://localhost:5000/news", {
+        axios.get("https://hawx-back.onrender.com/news", {
             headers: {
                 'Access-Control-Allow-Origin': '*',
                 'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
