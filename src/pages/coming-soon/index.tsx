207095a7132027f223c1965bca2180512bc53313
Alex Margaryan <margryanax@gmail.com>
2023-05-22 13:15:49 +0400

    coming soon

--- src/pages/coming-soon/index.tsx
+++ src/pages/coming-soon/index.tsx
@@ -12,7 +12,7 @@ const ComingSoon = () => {
           justifyContent:"center",
           flexDirection:"column",
           height:"100vh"
-      }}><p style={{color:"#0148FF"}}>Something good is coming soon!</p>
+      }}><p style={{color:"#0148FF"}}>Something amazing is coming soon!</p>
          <p style={{display:"flex", gap:"10px"}}> Stay tuned <img src={smile} alt="smile" style={{height:"48px"}}/></p></div>
   )
 }
