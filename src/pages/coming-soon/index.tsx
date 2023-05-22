6eeba2d04851e0c6ed8802067dd0992b9a967206
Alex Margaryan <margryanax@gmail.com>
2023-05-22 12:54:29 +0400

    coming soon

--- src/pages/coming-soon/index.tsx
+++ src/pages/coming-soon/index.tsx
@@ -1,15 +1,19 @@
+import smile from "src/assets/img/smile.png"
+
 const ComingSoon = () => {
   return(
       <div style={{
-          fontSize:"31px",
-          maxWidth:"500px",
+          fontSize:"32px",
+          // maxWidth:"500px",
           textAlign:"center",
           margin:"0 auto",
           display:"flex",
           alignItems:"center",
+          justifyContent:"center",
+          flexDirection:"column",
           height:"100vh"
-      }}>Something good is coming soon!
-          Stay tuned 🤩</div>
+      }}><p style={{color:"#0148FF"}}>Something good is coming soon!</p>
+         <p style={{display:"flex", gap:"10px"}}> Stay tuned <img src={smile} alt="smile" style={{height:"48px"}}/></p></div>
   )
 }
 export default ComingSoon
