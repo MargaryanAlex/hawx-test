6d53de77ffc05be52f9991d8aef2be5fe031c950
Alex Margaryan <margryanax@gmail.com>
2023-05-23 12:49:27 +0400

    coming soon with logo

--- src/pages/coming-soon/index.tsx
+++ src/pages/coming-soon/index.tsx
@@ -1,20 +1,25 @@
 import smile from "src/assets/img/smile.png"
+import logo from "src/assets/img/hawxAsset 2.svg"
 
 const ComingSoon = () => {
-  return(
-      <div style={{
-          fontSize:"32px",
-          // maxWidth:"500px",
-          padding:"0 20px",
-          textAlign:"center",
-          margin:"0 auto",
-          display:"flex",
-          alignItems:"center",
-          justifyContent:"center",
-          flexDirection:"column",
-          minHeight:"100vh"
-      }}><p style={{color:"#0148FF"}}>Something amazing is coming soon!</p>
-         <p style={{display:"flex", gap:"10px"}}> Stay tuned <img src={smile} alt="smile" style={{height:"48px"}}/></p></div>
-  )
+    return (
+        <div style={{
+            fontSize: "32px",
+            // maxWidth:"500px",
+            padding: "0 20px",
+            textAlign: "center",
+            margin: "0 auto",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            flexDirection: "column",
+            minHeight: "100vh"
+        }}><img src={logo} alt="" style={{
+            maxWidth: "500px",
+        }}/>
+            <p style={{color: "#0148FF"}}>Something amazing is coming soon!</p>
+            <p style={{display: "flex", gap: "10px"}}> Stay tuned <img src={smile} alt="smile"
+                                                                       style={{height: "48px"}}/></p></div>
+    )
 }
 export default ComingSoon
