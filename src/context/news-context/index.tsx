ec929be3ff335d4d897a103e864ae8aa3d96565d
Alex Margaryan <margryanax@gmail.com>
2024-03-01 12:17:10 +0400

    update

--- src/context/news-context/index.tsx
+++ src/context/news-context/index.tsx
@@ -1,48 +1,42 @@
-import {
-  createContext,
-  ReactElement,
-  useContext,
-  useEffect,
-  useState,
-} from "react";
-import { INewsModel } from "../../models/news.model";
-import NewsAPI from "../../utilits/api/newsApi";
-import { LoaderContext } from "../loader-context";
+import {createContext, ReactElement, useContext, useEffect, useState,} from "react";
+import {INewsModel} from "../../models/news.model";
+import {LoaderContext} from "../loader-context";
 import json from "../../utilits/hawx.news.json";
 
 interface INewsContext {
-  data: INewsModel[];
-  setData: (data: INewsModel[]) => void;
+    data: INewsModel[];
+    setData: (data: INewsModel[]) => void;
 }
 
 export const NewsContext = createContext<INewsContext>({
-  data: [],
-  setData: () => {},
+    data: [],
+    setData: () => {
+    },
 });
 
-const NewsProvider = ({ children }: { children: ReactElement }) => {
-  const [data, setData] = useState<INewsModel[]>(
-    json as unknown as INewsModel[]
-  );
-  const { setLoader } = useContext(LoaderContext);
+const NewsProvider = ({children}: { children: ReactElement }) => {
+    const [data, setData] = useState<INewsModel[]>(
+        json as unknown as INewsModel[]
+    );
+    const {setLoader} = useContext(LoaderContext);
 
-  useEffect(() => {
-    console.log(json);
-    setLoader(false);
+    useEffect(() => {
+        // console.log(json);
+        setLoader(false);
 
-    // NewsAPI.getNews().then((res) => {
-    //     let newData = [...res.data].sort((a, b) => a.order - b.order)
-    //     setData([...newData])
-    //     setLoader(false)
-    // }).catch((err) => {
-    //     console.log(err)
-    // })
-  }, []);
-  return (
-    <NewsContext.Provider value={{ data, setData }}>
-      {children}
-    </NewsContext.Provider>
-  );
+        // NewsAPI.getNews().then((res) => {
+        //     let newData = [...res.data].sort((a, b) => a.order - b.order)
+        //     setData([...newData])
+        //     setLoader(false)
+        // }).catch((err) => {
+        //     console.log(err)
+        // })
+    }, []);
+    return (
+        <NewsContext.Provider value={{data, setData}}>
+            {children}
+        </NewsContext.Provider>
+    );
 };
 
 export default NewsProvider;
